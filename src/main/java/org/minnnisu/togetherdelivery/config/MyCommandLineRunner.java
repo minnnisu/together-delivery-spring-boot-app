0df382fce4ca8640e86a3805bb69104bcdb60b59
minnnisu <mine52100@naver.com>
2024-04-03 14:58:45 +0900

    카테고리 자동삽입 기능 구현

---
+++ src/main/java/org/minnnisu/togetherdelivery/config/MyCommandLineRunner.java
@@ -0,0 +1,32 @@
+package org.minnnisu.togetherdelivery.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.minnnisu.togetherdelivery.constant.MealCategoryCode;
+import org.minnnisu.togetherdelivery.domain.Category;
+import org.minnnisu.togetherdelivery.repository.CategoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class MyCommandLineRunner implements CommandLineRunner {
+    final private CategoryRepository categoryRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        initializeDefaultCategory();
+        log.info("초기 카테고리 데이터 생성");
+    }
+
+    public void initializeDefaultCategory(){
+        for (MealCategoryCode mealCategoryCode : MealCategoryCode.values()) {
+            categoryRepository.save(
+                    Category.of(
+                            mealCategoryCode.name(),
+                            mealCategoryCode.getCategoryName())
+            );
+        }
+    }
+}
---
+++ src/main/java/org/minnnisu/togetherdelivery/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package org.minnnisu.togetherdelivery.repository;
+
+import org.minnnisu.togetherdelivery.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByCategoryCode(String categoryCode);
+}
